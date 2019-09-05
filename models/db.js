0c693c2bdfbfd2910b6c89e76bab2af8e66470a8
ahmedelgendyfci <ahmedelgendyfci@gmail.com>
2019-09-05 16:48:47 +0200

    db

--- models/db.js
+++ models/db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI || "mongodb://localhost:27017"
 
-mongoose.connect(url+"/heroku_7hgb85dk")
+mongoose.connect(url+"/heroku_5313h20l")
